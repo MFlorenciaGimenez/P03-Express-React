5afbe2238d92accac62a46496b428d8312933044
MFlorenciaGimenez <florgimenez14@hotmail.com>
2025-10-24 14:50:01 -0300

    extra ,

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -1,18 +1,20 @@
 import { DataSource } from "typeorm";
 import { User } from "../entities/User";
 import { Reservation } from "../entities/Reservation";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./envs";
 import { Credential } from "../entities/Credential";
 import { MenuItem } from "../entities/MenuItems";
 import { RestaurantTable } from "../entities/Table";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  url: process.env.DATABASE_URL, ,
+  url: process.env.DATABASE_URL,
   synchronize: true,
   dropSchema: false,
   logging: false,
-   ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
   entities: [User, Reservation, Credential, MenuItem, RestaurantTable],
   subscribers: [],
   migrations: [],
